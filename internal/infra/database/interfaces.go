3e8d64d3d11f8932ca7329b9aa219f9bf79e8b9f
misael <misael.geof@gmail.com>
2022-10-23 20:09:36 -0300

    add product interface

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -7,3 +7,10 @@ type UserInterface interface {
 	FindByEmail(emailId string) (*entity.User, error)
 }
 
+type ProductInterface interface {
+	Create(product *entity.Product) error
+	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	FindByID(id string)(*entity.Product, error)
+	Update(product *entity.Product) error
+	Delete(id string) error
+}
