cf80be9a044b6ae3791e0b893a058b34941d17f7
misael <misael.geof@gmail.com>
2022-10-23 20:54:51 -0300

    add products test

--- internal/infra/database/product_db_test.go
+++ internal/infra/database/product_db_test.go
@@ -17,10 +17,10 @@ func TestCreateNewProduct(t *testing.T) {
 		t.Error(err)
 	}
 	db.AutoMigrate((&entity.Product{}))
-	
+
 	product, err := entity.NewProduct("Product 1", 10.00)
 	assert.NoError(t, err)
-	
+
 	productDB := NewProduct(db)
 	err = productDB.Create(product)
 	assert.NoError(t, err)
@@ -28,23 +28,48 @@ func TestCreateNewProduct(t *testing.T) {
 
 }
 
-func TestFindAllProducts(t *testing.T){
+func TestFindAllProducts(t *testing.T) {
 	db, err := gorm.Open(sqlite.Open("file:memory:"), &gorm.Config{}) // no cache here for the tests
 	if err != nil {
 		t.Error(err)
 	}
 	db.AutoMigrate((&entity.Product{}))
-	
+
 	for i := 1; i < 24; i++ {
 		product, err := entity.NewProduct(fmt.Sprintf("Product %d", i), rand.Float64()*100)
 		assert.NoError(t, err)
 		db.Create(product)
 	}
-	
+
 	productDB := NewProduct(db)
 	products, err := productDB.FindAll(1, 10, "asc")
 	assert.NoError(t, err)
 	assert.Len(t, products, 10)
 	assert.Equal(t, "Product 1", products[0].Name)
 	assert.Equal(t, "Product 10", products[9].Name)
+
+	products, err = productDB.FindAll(2, 10, "asc")
+	assert.NoError(t, err)
+	assert.Len(t, products, 10)
+	assert.Equal(t, "Product 11", products[0].Name)
+	assert.Equal(t, "Product 20", products[9].Name)
 }
+
+func TestFindProductByID(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file:memory:"), &gorm.Config{}) // no cache here for the tests
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate((&entity.Product{}))
+	product, err := entity.NewProduct("Product 1", 20.0)
+	assert.NoError(t, err)
+	db.Create(product)
+	productDB := NewProduct(db)
+
+	product, err = productDB.FindByID(product.ID.String())
+
+	assert.NoError(t, err)
+	assert.Equal(t, "Product 1", product.Name)
+}
+
+
